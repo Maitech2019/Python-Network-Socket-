83986084065a04957d18672e692c885c04b1128a
Maitech2019 <45570848+Maitech2019@users.noreply.github.com>
2022-02-14 22:31:12 -0500

    Add files via upload

---
+++ basic-client.py
@@ -0,0 +1,18 @@
+#basic-client.py
+
+import socket
+
+server_ip = '192.168.1.156'
+port = 7000
+
+while True:
+	data = input("Enter Message: ")
+	server = socket.socket()
+	server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,1)
+
+	server.connect((server_ip,port))# this case my own ip
+	server.send(data.encode('utf-8'))#send data to server, need to encode utf-8
+
+	data_server = server.recv(1024).decode('utf-8')# max byt receive 1024 and decode to send in Thai utf-8
+	print('Data from server: ', data_server)
+	server.close()
